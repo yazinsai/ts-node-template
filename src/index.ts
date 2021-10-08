3eb2a0835e8b892d9097a827c0d8b31539b2a0b4
Yazin <github-7itbcg5@yaz.in>
2021-10-08 16:27:38 +0300

    Added missing dep

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import express, { Request, Response } from 'express';
-const PORT = process.env.PORT || 3000
+const port = process.env.PORT || "3000"
 const app = express()
 
 // Setup express
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
+  res.send('Hello World')
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`)
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
 })
