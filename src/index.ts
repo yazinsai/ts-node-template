502638d162f9aa7349692165ebd8a8dfd9c71fa6
Yazin <github-7itbcg5@yaz.in>
2021-10-08 16:23:19 +0300

    Add server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log(';)')
+import express, { Request, Response } from 'express';
+const PORT = process.env.PORT || 3000
+const app = express()
+
+// Setup express
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+})
